1213479180b989c6670525c1f2858355869348d7
Viktor-sun <bobick.zdoh@gmail.com>
2021-04-09 22:55:26 +0300

    add form from MATERIAL-UI

--- src/pages/RegisterPage/RegisterPage.js
+++ src/pages/RegisterPage/RegisterPage.js
@@ -1,9 +1,137 @@
 import React, { Component } from 'react';
+// import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Box from '@material-ui/core/Box';
+// import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+// import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+// const useStyles = makeStyles((theme) => ({
+//   paper: {
+//     marginTop: theme.spacing(8),
+//     display: 'flex',
+//     flexDirection: 'column',
+//     alignItems: 'center',
+//   },
+//   avatar: {
+//     margin: theme.spacing(1),
+//     backgroundColor: theme.palette.secondary.main,
+//   },
+//   form: {
+//     width: '100%', // Fix IE 11 issue.
+//     marginTop: theme.spacing(3),
+//   },
+//   submit: {
+//     margin: theme.spacing(3, 0, 2),
+//   },
+// }));
 
 class RegisterPage extends Component {
-  state = {};
+  state = { nickname: '', email: '', password: '' };
+
+  handleChange = e => {
+    const { name, value } = e.currentTarget;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    // this.props.onSubmit(this.state);
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({
+      nickname: '',
+      email: '',
+      password: '',
+    });
+  };
   render() {
-    return <h2>RegisterPage</h2>;
+    const { nickname, email, password } = this.state;
+
+    return (
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div>
+          {/* <Avatar>
+            <LockOutlinedIcon />
+          </Avatar> */}
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <form noValidate>
+            <TextField
+              autoComplete="fname"
+              name="nickname"
+              onChange={this.handleChange}
+              value={nickname}
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="nickname"
+              label="Nickname"
+              autoFocus
+            />
+
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              onChange={this.handleChange}
+              value={email}
+              autoComplete="email"
+            />
+
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              onChange={this.handleChange}
+              value={password}
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+
+            <Button type="submit" fullWidth variant="contained" color="primary">
+              Sign Up
+            </Button>
+          </form>
+        </div>
+        <Box mt={5}>
+          <Copyright />
+        </Box>
+      </Container>
+    );
   }
 }
 
