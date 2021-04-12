43a15bd064504914b428094a34da930c0bb7b472
Viktor-sun <bobick.zdoh@gmail.com>
2021-04-12 23:58:04 +0300

    add menu from material-ui

--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -1,16 +1,44 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import IconButton from '@material-ui/core/IconButton';
 import { connect } from 'react-redux';
 import { authSelectors, authOperations } from '../../redux/auth';
+import './UserMenu.scss';
 
 const UserMenu = ({ name, onLogout }) => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <>
-      <span>Welcome, {name}</span>
+      <IconButton
+        color="inherit"
+        onClick={handleClick}
+        aria-label="open user menu"
+      >
+        <AccountCircleIcon fontSize="large" />
+      </IconButton>
 
-      <Button variant="contained" color="secondary" onClick={onLogout}>
-        Logout
-      </Button>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>
+          <span className="UserMenu-name"> {name}</span>
+        </MenuItem>
+        <MenuItem onClick={onLogout}>Logout</MenuItem>
+      </Menu>
     </>
   );
 };
