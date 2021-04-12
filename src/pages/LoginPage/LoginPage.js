275761b30ac90d0496314cf3036b2c4724b6cd85
Viktor-sun <bobick.zdoh@gmail.com>
2021-04-13 01:10:31 +0300

    add icon

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-// import Avatar from '@material-ui/core/Avatar';
+import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import Link from '@material-ui/core/Link';
 import Box from '@material-ui/core/Box';
-// import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 // import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
@@ -75,9 +75,9 @@ class LoginPage extends Component {
       <Container component="main" maxWidth="xs">
         <CssBaseline />
         <div>
-          {/* <Avatar className={classes.avatar}>
+          <Avatar>
             <LockOutlinedIcon />
-          </Avatar> */}
+          </Avatar>
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
--- src/pages/RegisterPage/RegisterPage.js
+++ src/pages/RegisterPage/RegisterPage.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-// import Avatar from '@material-ui/core/Avatar';
+import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import Link from '@material-ui/core/Link';
 import Box from '@material-ui/core/Box';
-// import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 // import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
@@ -76,9 +76,9 @@ class RegisterPage extends Component {
       <Container component="main" maxWidth="xs">
         <CssBaseline />
         <div>
-          {/* <Avatar>
+          <Avatar>
             <LockOutlinedIcon />
-          </Avatar> */}
+          </Avatar>
           <Typography component="h1" variant="h5">
             Sign up
           </Typography>
