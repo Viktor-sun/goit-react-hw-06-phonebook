c30257149acf6fb0f8587843153e198d9196c945
Viktor-sun <bobick.zdoh@gmail.com>
2021-04-13 00:53:40 +0300

    add props, update

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './Title.scss';
 
-const Title = () => (
+const Title = ({ title }) => (
   <div className="wrapper-title">
-    <h1 className="title">Phonebook</h1>
+    <h1 className="title">{title}</h1>
   </div>
 );
 
