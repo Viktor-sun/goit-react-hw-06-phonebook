7322f024d617f8fcf6bb81277ba56b9907a7f7ba
Viktor-sun <bobick.zdoh@gmail.com>
2021-04-13 01:11:26 +0300

    add content

--- src/pages/DoNotAuthenticated/DoNotAuthenticated.js
+++ src/pages/DoNotAuthenticated/DoNotAuthenticated.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Container from '@material-ui/core/Container';
+import Title from '../../components/Title';
+import routes from '../../routes';
+import './DoNotAuthenticated.scss';
 
 const DoNotAuthenticated = () => {
-  return <h1>Please Authenticated!!!</h1>;
+  return (
+    <Container fixed>
+      <Title title="Please Authenticated!"></Title>
+      <p className="DoNotAuthenticated__description">
+        You need to <NavLink to={routes.login}>log in</NavLink> or{' '}
+        <NavLink to={routes.register}>log up</NavLink> to use the phone book.
+      </p>
+    </Container>
+  );
 };
 
 export default DoNotAuthenticated;
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Container from '@material-ui/core/Container';
+import Title from '../../components/Title';
+import routes from '../../routes';
+import './HomePage.scss';
 
-const HomePage = () => <h1>Welcome!</h1>;
+const HomePage = () => (
+  <Container fixed>
+    <Title title="Phonebook" />
 
+    <p className="home-welcome">
+      Welcome to your new phonebook!{' '}
+      <NavLink to={routes.contacts}>Go to contacts</NavLink>
+    </p>
+  </Container>
+);
 export default HomePage;
